d6fac16bd72e6f2b040a2801e25c6ff1278eb2d5
Simón Benítez Soto <135668710+SimnBentez@users.noreply.github.com>
2023-07-06 15:30:38 -0500

    Add files via upload

---
+++ Sum of natural numbers raised to any power.py
@@ -0,0 +1,18 @@
+def suman(i,n):
+    j = 0
+    for k in range(0,i+1):
+        j = j+k**n
+    return j
+
+i = int(input('Por favor seleccione los primeros n números que desea sumar a un exponente n: '))
+if i<0:
+    while i<0:
+        print('Es imposible sumar los primeros números negativos empezando desde cero \n')
+        i = int(input('Por favor seleccione los primeros n números que desea sumar a un exponente n: '))
+    n = int(input(f'Por favor seleccione el exponente que desea sumar los {i} números naturales: '))
+else:
+    n = int(input(f'Por favor seleccione el exponente que desea sumar los {i} números naturales: '))
+
+l = suman(i,n)
+
+print(f'La suma de los primeros {i} números naturales elevados a {n} es: {l}')
